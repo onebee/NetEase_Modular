7b8cc96230c48c2bfdf425fa213fed131219bcb6
Kaibin <diaokaibin@gmail.com>
2019-12-22 22:36:29 +0800

    update

---
+++ personal/src/main/java/com/one/netease/personal/Personal_MainActivity.java
@@ -0,0 +1,16 @@
+package com.one.netease.personal;
+
+import android.os.Bundle;
+
+import com.one.netease.order.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Personal_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_activity_main);
+    }
+}
